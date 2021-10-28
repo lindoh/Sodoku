52b45f48924343c7c125a6407ccacbc85cb22efd
Lindo Gamede <lindohgamede@gmail.com>
2021-10-28 12:34:26 +0200

    Added the function to check validity of each column

--- Sodoku/Sodoku.py
+++ Sodoku/Sodoku.py
@@ -19,23 +19,40 @@ Board2 = ['195743862',
           '928671354',
           '254938671']
 
-#The digits to use to check the validity of the board
-digits = '123456789'
-
 #Check the validity of each row
 def validate_rows (Board):
     for row in Board:
-        for digit in digits:
-            if row.find(digit) == -1:   #Check if you find all the digits from each row
+        for digit in range(1, 10):
+            if row.find(str(digit)) == -1:   #Check if you find all the digits from each row
                 return False
-            if row.count(digit) > 1:    #Check if a digit is counted more than once
+            if row.count(str(digit)) > 1:    #Check if a digit is counted more than once
                 return False
     #If all is well then return true
     return True
 
+#Check the validity of each column
+def validate_column(Board):
+    digits = ''
+    column = []
+
+    #Create a Board of columns in a horizontal form for ease of use
+    for i in range(9):
+        for row in Board: 
+            digits += row[i]
+        column.append(digits)
+        digits = ''
+
+    #The validate_row function can be used to check each column
+    valid = validate_rows(column)
+    return valid
+
+
+
+
 
 #------------Sample Test------------------------------------------------------------------------
-if validate_rows(Board1):
+if validate_rows(Board2) and validate_column(Board2):
     print('Yes')
 else:
     print('No')
+
