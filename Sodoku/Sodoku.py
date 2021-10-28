75acfb2ec5ed66d595075df684e43d1d76616463
Lindo Gamede <lindohgamede@gmail.com>
2021-10-28 12:02:38 +0200

    Made first function test

--- Sodoku/Sodoku.py
+++ Sodoku/Sodoku.py
@@ -1,1 +1,41 @@
+#Create the Sodoku board(s)
+Board1 = ['295743861',
+          '431865927',
+          '876192543',
+          '387459216',
+          '612387495',
+          '549216738',
+          '763524189',
+          '928671354',
+          '154938672']
 
+Board2 = ['195743862',
+          '431865927',
+          '876192543',
+          '387459216',
+          '612387495',
+          '549216738',
+          '763524189',
+          '928671354',
+          '254938671']
+
+#The digits to use to check the validity of the board
+digits = '123456789'
+
+#Check the validity of each row
+def validate_rows (Board):
+    for row in Board:
+        for digit in digits:
+            if row.find(digit) == -1:   #Check if you find all the digits from each row
+                return False
+            if row.count(digit) > 1:    #Check if a digit is counted more than once
+                return False
+    #If all is well then return true
+    return True
+
+
+#------------Sample Test------------------------------------------------------------------------
+if validate_rows(Board1):
+    print('Yes')
+else:
+    print('No')
