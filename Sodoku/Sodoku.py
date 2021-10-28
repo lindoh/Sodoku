96b209acf4286f4bfa5b123d2d9729413b36ef23
Lindo Gamede <lindohgamede@gmail.com>
2021-10-28 15:57:49 +0200

    Check the validity of each 3X3 sub-square. Without comments!!

--- Sodoku/Sodoku.py
+++ Sodoku/Sodoku.py
@@ -19,8 +19,8 @@ Board2 = ['195743862',
           '928671354',
           '254938671']
 
-#Check the validity of each row
-def validate_rows (Board):
+#--------------Check the validity of each row---------------
+def validate_row(Board):
     for row in Board:
         for digit in range(1, 10):
             if row.find(str(digit)) == -1:   #Check if you find all the digits from each row
@@ -30,7 +30,7 @@ def validate_rows (Board):
     #If all is well then return true
     return True
 
-#Check the validity of each column
+#--------------Check the validity of each column---------------
 def validate_column(Board):
     digits = ''
     column = []
@@ -43,16 +43,43 @@ def validate_column(Board):
         digits = ''
 
     #The validate_row function can be used to check each column
-    valid = validate_rows(column)
+    valid = validate_row(column)
     return valid
 
+#--------------Check the validity of each 3X3 sub-square---------------
+def validate_sub_sqr(Board):
+    new_board = [[elem for elem in row] for row in Board]
+    mask = ''
+    r_start = c_start = 0
+    r_end = c_end = 3
 
+    while r_end < 10:
+        for i in range(r_start, r_end):
+            for j in range(c_start, c_end):
+                mask += new_board[i][j]
+        
+        for digit in range(1, 10):
+            if mask.find(str(digit)) == -1:
+                return False
+            if mask.count(str(digit)) > 1:
+                return False
+        mask = ''
+        c_start += 3
+        c_end += 3
+        if c_end > 10:
+            r_start += 3
+            r_end += 3
+            c_start = 0
+            c_end = 3
 
+    #If all is well return True
+    return True
 
 
 #------------Sample Test------------------------------------------------------------------------
-if validate_rows(Board2) and validate_column(Board2):
+if validate_row(Board2) and validate_column(Board2) and validate_sub_sqr(Board2):
     print('Yes')
 else:
     print('No')
+
 
